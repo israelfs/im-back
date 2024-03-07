eaf7ec45bafab37df90cdcfb43fbb1c373ec184a
Israel <israelfelipe9@gmail.com>
2024-03-07 11:05:52 -0300

    feat: add websocket

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 import express from "express";
+import http from "http";
+import { WebSocketServer } from "ws";
 import bodyParser from "body-parser";
 import todoRouter from "./routers/todoRouter.js";
 import cors from "cors";
 
 const app = express();
+const server = http.createServer(app);
+
+const wsServer = new WebSocketServer({ server });
+
+wsServer.on("connection", (socket) => {
+  console.log("A new client connected");
+  socket.send("Welcome to the server!");
+
+  socket.on("message", (msg) => {
+    msg = msg.toString();
+    wsServer.clients.forEach((client) => {
+      if (client !== socket) {
+        client.send(msg);
+      }
+    });
+  });
+});
 
 app.use(
   bodyParser.urlencoded({
@@ -21,6 +40,6 @@ app.use((err, req, res, next) => {
   res.status(500).send("Something broke!");
 });
 
-app.listen(3000, () => {
+server.listen(3000, () => {
   console.log("Example app listening on port 3000!");
 });
